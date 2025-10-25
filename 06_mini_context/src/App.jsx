687fe28d4e4ae5182573dfb5ee4393a541b08eea
Shobhit150 <vshobhit150@gmail.com>
2025-10-26 03:30:59 +0530

    update: user context api 1

--- 06_mini_context/src/App.jsx
+++ 06_mini_context/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [cou, setcou] = useState(2)
-  const ok = () => {
-    setcou(prev => prev + 1)
-    setcou(prev => prev + 1)
-    
-  }
+  
   return (
     <UserContextProvider>
-      <h1>React Global variable</h1>
-      <button className='' onClick={ok}>ok {cou} </button>
+      
+      <h1>Login</h1>
+      <Login/>
+      <Profile/>
 
     </UserContextProvider>
   )
